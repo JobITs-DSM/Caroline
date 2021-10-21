0a36ec98f8a527a07e2093044b2e9589785a87bc
jiwon <jidole041214@naver.com>
2021-10-21 20:27:22 +0900

    add profile card

--- components/common/ProfileCard/index.tsx
+++ components/common/ProfileCard/index.tsx
@@ -2,7 +2,18 @@ import s from "./ProfileCard.module.css";
 import { FC } from "react";
 
 const ProfileCard: FC = () => {
-  return <div className={s.wrapper}></div>;
+  return (
+    <div className={s.wrapper}>
+      <div className={s.profile_infor}>
+        <img src="http://image.kmib.co.kr/online_image/2020/0122/202001220402_11170924119205_1.jpg" />
+        <div>
+          <h1>김팔복</h1>
+          <h3>프론트엔드</h3>
+        </div>
+      </div>
+      <button>지원하기</button>
+    </div>
+  );
 };
 
 export default ProfileCard;
--- components/companyParticular/Review.tsx
+++ components/companyParticular/Review.tsx
@@ -5,7 +5,7 @@ const Review: FC = () => {
   return (
     <div>
       <img src="http://t1.daumcdn.net/liveboard/subusunews/ecfd52e3c81a492abbf4e7d6191e36e9.png" />
-      <p>솔직히 이번 면접 너무 어렵네요,,, 이렇게 어려운 적이 있었니??</p>
+      <p>솔직히 이번 면접 너무 어렵네요... 이런 면접이 있엇나요??</p>
     </div>
   );
 };
